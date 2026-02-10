7e52030eb75e6e62c0aa0f0ae4925ac1112fd979
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-10 22:29:42 +0700

    datatype

---
+++ datatype/datatype.js
@@ -0,0 +1,22 @@
+console.log("datatype");
+
+//number
+const score = 20;
+const realScore = 3.4;
+console.log(score, realScore);
+
+//string
+const name = "Nariyuki";
+console.log(name);
+
+//boolean
+const isSuccess = true;
+console.log(isSuccess);
+
+//undefined
+const Undefined = 1;
+console.log(Undefined);
+
+//null
+const testNull = null;
+console.log(testNull);
