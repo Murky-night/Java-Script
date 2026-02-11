7176375c04f6806703764b6f7c1738585522947a
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-11 10:52:22 +0700

    Operator

---
+++ operators/ope.js
@@ -0,0 +1,19 @@
+console.log("Operator");
+
+let a = 6;
+let b = ++a;
+// let c = a++;
+console.log(a, b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+const c = 5
+const d = "5"
+console.log(c == d);
+console.log(c === d);
+console.log(c != d);
+console.log(c !== d);
