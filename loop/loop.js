c21243f7fe11bcf91f2dcdbd455077af7703337a
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-12 08:58:00 +0700

    Loop

---
+++ loop/loop.js
@@ -0,0 +1,19 @@
+console.log("Loop");
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i = ${i}`);
+}
+
+let score = 10 
+while (
+    score >= 5
+) {
+    console.log("Score: ",score);
+    score--;
+}   
+
+let score1 = 10;
+do {
+    console.log("Score1:",score1);
+    score1--;
+} while (score1 > 5);
