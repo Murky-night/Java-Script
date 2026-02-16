e6599476258b2673ce2e86860771390a77326ce2
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-16 18:28:53 +0700

    for-in for-each

--- array&object/for-in/forin.js
+++ array&object/for-in/forin.js
@@ -1,2 +1,39 @@
 console.log("For-in");
 
+const sv1 = {
+    name: "Khang",
+    age: 18,
+    address: "Long Bien"
+}
+
+const sv2 = {
+    name: "Lam",
+    age: 19,
+    address: "Ha Dong"
+}
+
+const sv3 = {
+    name: "Nam",
+    age: 20,
+    address: "Cau Giay"
+}
+
+const sinhVien = [sv1, sv2, sv3];
+console.log(sinhVien);
+
+sinhVien.forEach((item,index) => {
+    console.log("index =",index,"name =",item.name);
+});
+
+console.log("========================");
+
+for (const key in sv1) {
+    console.log(key,sv1[key]);
+}
+
+console.log("========================");
+
+for (let item of Object.values(sv1)) {
+    console.log(item);   
+}
+
